bfdeea00a68ca462e14ac6f958e3289f4d1ee24f
Muhammad <msalah.29.10@gmail.com>
2024-02-06 08:48:51 +0000

    update bundle script various OS

--- bundle.sh
+++ bundle.sh
@@ -1,17 +1,38 @@
-rm -r ./dist
-pyinstaller -F launcher.py --name SGLICollect \
-    -p . \
-    -p src \
-    -p src/extractors \
-    -p src/gportal \
-    -p src/gportal/gportal_types \
-    -p src/jasmes \
-    -p src/jasmes/jasmes_types \
-    --hidden-import src \
-    --hidden-import io \
-    --hidden-import io.open \
-    --collect-all src \
-    --noconfirm
-
-mkdir ./dist/SGLICollect/temp
-touch ./dist/SGLICollect/temp/.gitkeep
+unameOut="$(uname -s)"
+case "${unameOut}" in
+    Linux*)     machine=Linux;;
+    Darwin*)    machine=Mac;;
+    CYGWIN*)    machine=Cygwin;;
+    MINGW*)     machine=MinGw;;
+    *)          machine="UNKNOWN:${unameOut}"
+esac
+if [ $machine = "Linux" ] 
+then
+    out="./dist/linux"
+elif [ $machine = "Mac" ] 
+then
+    out="./dist/mac"
+elif [ $machine = "MinGw" ] 
+then
+    out="./dist/mingw"
+elif [ $machine = "./dist/cygwin" ] 
+then
+    out="./dist/cygwin"
+else
+    echo "unsupported OS"
+    exit
+fi
+    rm -r $out
+    pyinstaller -F launcher.py --name SGLICollect --distpath $out\
+        -p . \
+        -p src \
+        -p src/extractors \
+        -p src/gportal \
+        -p src/gportal/gportal_types \
+        -p src/jasmes \
+        -p src/jasmes/jasmes_types \
+        --hidden-import src \
+        --hidden-import io \
+        --hidden-import io.open \
+        --collect-all src \
+        --noconfirm
