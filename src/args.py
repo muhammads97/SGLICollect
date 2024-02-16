2980856c1a8a5e25122db0504fa33f0a81424e13
Muhammad Salah <msalah.29.10@gmail.com>
2024-02-16 16:35:25 +0900

    update version number

--- src/args.py
+++ src/args.py
@@ -17,10 +17,10 @@ from io import open
 import os
 import tempfile
 
-__version__ = "1.1.1"
+__version__ = "1.1.2"
 dirs = __file__.split("/")
 dir_index = -1
-for i,d in enumerate(dirs):
+for i, d in enumerate(dirs):
     if d == "SGLICollect":
         dir_index = i
 
@@ -28,41 +28,89 @@ for i,d in enumerate(dirs):
 TEMP_FOLDER = Path(os.path.join(tempfile.gettempdir(), "SGLICollect"))
 TEMP_FOLDER.mkdir(exist_ok=True)
 
-parser = argparse.ArgumentParser(description="Welcome to SGLICollect!\n"
-                                             "This API is developed for searching and downloading SGLI Images.\n" 
-                                             "This API is developed by Muhammad Salah (msalah.29.10@gmail.com)\n",
-                                formatter_class=argparse.RawTextHelpFormatter
-                                )
-parser.add_argument('--version', action='version',
-                    version='%(prog)s {version}'.format(version=__version__))
-parser.add_argument('-s', '--search', action="store_true", help='Search GPortal for products')
-parser.add_argument('-d', '--download', action="store_true", help='Download the products')
-parser.add_argument('-e', "--extract", action="store_true", help="extract the pixel matching the lat, and long from the product")
+parser = argparse.ArgumentParser(
+    description="Welcome to SGLICollect!\n"
+    "This API is developed for searching and downloading SGLI Images.\n"
+    "This API is developed by Muhammad Salah (msalah.29.10@gmail.com)\n",
+    formatter_class=argparse.RawTextHelpFormatter,
+)
+parser.add_argument(
+    "--version",
+    action="version",
+    version="%(prog)s {version}".format(version=__version__),
+)
+parser.add_argument(
+    "-s", "--search", action="store_true", help="Search GPortal for products"
+)
+parser.add_argument(
+    "-d", "--download", action="store_true", help="Download the products"
+)
+parser.add_argument(
+    "-e",
+    "--extract",
+    action="store_true",
+    help="extract the pixel matching the lat, and long from the product",
+)
 
-parser.add_argument("--date", nargs="?",
-                    help="sample date format: yyyy/mm/dd", type=str)
-parser.add_argument('-p', "--product", nargs="?",
-                    help="'L1B' for level 1, 'L2R' for level 2 NWLR (Rrs), or 'L2P' for level 2 IWLR (Chla, etc)",
-                    type=str, default="L1B")
-parser.add_argument('-lat', "--latitude", nargs="?",
-                    help="Latitude", type=float)
-parser.add_argument('-lon', "--longitude", nargs="?",
-                    help="Longitude", type=float)
-parser.add_argument('-r', "--resolution", nargs="?", type=str,
-                    default="250m", help="resolution for GPortal (250m or 1000m)")
-parser.add_argument('--api', nargs="?", type=SGLIAPIs, default=SGLIAPIs.GPORTAL,
-                    help="API: GPORTAL or JASMES (Not implemented yet)")
-parser.add_argument('-c', "--config-file", type=Path,
-                    help="provide the arguments in a json file")
-parser.add_argument('--csv', nargs="?", type=Path,
-                    help="a path to a json file containing the search results")
-parser.add_argument("--no-repeat", action="store_true", help="don't repeat if identifier exists")
-parser.add_argument("--cred", type=Path, help="a path to json file containing account and password\nexample: \n{\n\t\"account\": \"<YOUR_USERNAME>\", \n\t\"password\": \"<YOUR_PASSOWRD>\"\n}")
-parser.add_argument("--download-dir", type=Path, default=TEMP_FOLDER, help="directory path of the download location")
+parser.add_argument(
+    "--date", nargs="?", help="sample date format: yyyy/mm/dd", type=str
+)
+parser.add_argument(
+    "-p",
+    "--product",
+    nargs="?",
+    help="'L1B' for level 1, 'L2R' for level 2 NWLR (Rrs), or 'L2P' for level 2 IWLR (Chla, etc)",
+    type=str,
+    default="L1B",
+)
+parser.add_argument("-lat", "--latitude", nargs="?", help="Latitude", type=float)
+parser.add_argument("-lon", "--longitude", nargs="?", help="Longitude", type=float)
+parser.add_argument(
+    "-r",
+    "--resolution",
+    nargs="?",
+    type=str,
+    default="250m",
+    help="resolution for GPortal (250m or 1000m)",
+)
+parser.add_argument(
+    "--api",
+    nargs="?",
+    type=SGLIAPIs,
+    default=SGLIAPIs.GPORTAL,
+    help="API: GPORTAL or JASMES (Not implemented yet)",
+)
+parser.add_argument(
+    "-c", "--config-file", type=Path, help="provide the arguments in a json file"
+)
+parser.add_argument(
+    "--csv",
+    nargs="?",
+    type=Path,
+    help="a path to a json file containing the search results",
+)
+parser.add_argument(
+    "--no-repeat", action="store_true", help="don't repeat if identifier exists"
+)
+parser.add_argument(
+    "--cred",
+    type=Path,
+    help='a path to json file containing account and password\nexample: \n{\n\t"account": "<YOUR_USERNAME>", \n\t"password": "<YOUR_PASSOWRD>"\n}',
+)
+parser.add_argument(
+    "--download-dir",
+    type=Path,
+    default=TEMP_FOLDER,
+    help="directory path of the download location",
+)
 parser.add_argument("--download-url", type=str, help="url of product to download")
-parser.add_argument("--ftp-path", type=str, help="path of product to download on the ftp server")
+parser.add_argument(
+    "--ftp-path", type=str, help="path of product to download on the ftp server"
+)
 parser.add_argument("--product-path", type=Path, help="product path for extract option")
-parser.add_argument("--product-dir", type=Path, help="directory containing products to extract")
+parser.add_argument(
+    "--product-dir", type=Path, help="directory containing products to extract"
+)
 args, _ = parser.parse_known_args()
 
 if args.config_file:
@@ -74,11 +122,11 @@ if args.config_file:
     for k in config["args"].keys():
         if not isinstance(getattr(args, k), type(None)):
             setattr(args, k, type(getattr(args, k))(config["args"][k]))
-        else:#
-# Created on Sat Dec 16 2023
-#
-# Copyright (c) 2023 Your Company
-#
+        else:  #
+            # Created on Sat Dec 16 2023
+            #
+            # Copyright (c) 2023 Your Company
+            #
 
             setattr(args, k, config["args"][k])
 if not args.search and not args.download and not args.extract:
@@ -93,12 +141,11 @@ if args.search:
         if args.latitude == None or args.longitude == None:
             print("latitude and longitude must be provided")
             exit(1)
-if args.cred == None: 
+if args.cred == None:
     print("credentials must be provided via a json file")
     exit(1)
 if args.extract:
     if not args.csv:
         if args.latitude == None or args.longitude == None:
             print("latitude and longitude must be provided")
-            exit(1)   
-
+            exit(1)
